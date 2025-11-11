7d43ff44d4b3ab92e7fbe3958f5ee0e8d70a2632
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-11-11 15:02:07 +0200

    Add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,6 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const baseCostForDay = 40;
+
+  if (days >= 7) {
+    return baseCostForDay * days - 50;
+  } else if (days >= 3 && days <= 6) {
+    return baseCostForDay * days - 20;
+  } else if (days <= 2) {
+    return baseCostForDay * days;
+  }
   // write code here
 }
 
